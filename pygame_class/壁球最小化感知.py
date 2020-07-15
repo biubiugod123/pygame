46c08895f21915a903769c2905a6e3597d25077e
biubiu <biubiugod@gmail.com>
2020-07-15 06:27:45 -0400

    resizable sense

---
+++ pygame_class/壁球最小化感知.py
@@ -0,0 +1,49 @@
+# pygame hello world ball game screen size resizable 2.0
+import pygame, sys
+
+pygame.init()
+
+size = width, height =600, 400
+
+speed = [1,1]
+Black = 0,0,0
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+pygame.display.set_caption('python ball game')
+ball = pygame.image.load("PYG02-ball.gif")
+ballrect = ball.get_rect()
+FPS = 300
+fclock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed[0] = speed[0] if speed[0] == 0 else (abs(speed[0])-1) * int(speed[0]/abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0] = speed[0] + 1 if speed[0] > 0 else speed[0]-1
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1]-1
+            elif event.key == pygame.K_DOWN:
+                speed[1] = speed[1] if speed[1] == 0 else (abs(speed[0])-1) * int(speed[0]/abs(speed[0]))
+            elif event.key == pygame.K_ESCAPE:
+                sys.exit()
+        elif event.type == pygame.VIDEORESIZE:
+            size = width, height = event.size[0], size[1]
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    if pygame.display.get_active():
+
+        ballrect = ballrect.move(speed[0], speed[1])
+
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = - speed[1]
+
+
+    screen.fill(Black)
+    screen.blit(ball,ballrect)
+    pygame.display.update()
+    fclock.tick(FPS)
