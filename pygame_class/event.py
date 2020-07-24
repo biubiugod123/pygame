a53d7368368f2b17a012508fea9f5eeb0d2ef6e4
biubiu <biubiugod@gmail.com>
2020-07-24 12:31:49 -0400

    event test

---
+++ pygame_class/event.py
@@ -0,0 +1,17 @@
+# pygame 事件处理
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption('python event')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.unicode == "":
+                print("[KEYDOWN]:", "#",event.key, event.mod)
+            else:
+                print("[KEYDOWN]",event.unicode,event.key,event.mod)
+    pygame.display.update()
